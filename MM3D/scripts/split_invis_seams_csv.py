996edead6c01cfe42c224ca489ff42e92fd6c193
Exodus <7614885+exodus122@users.noreply.github.com>
2025-12-13 20:48:16 -0500

    Draw 2D invisible seams for OOT3D and MM3D

--- MM3D/scripts/split_invis_seams_csv.py
+++ MM3D/scripts/split_invis_seams_csv.py
@@ -1,7 +1,7 @@
 import csv
 from collections import defaultdict
 
-input_csv = "oot.csv"  # change this to your filename
+input_csv = "mm3d_new_07.csv"  # change this to your filename
 scenes = defaultdict(list)
 
 # Read CSV and group rows by column 2
